dbbaa270c2d2863e39a0729b5812a8113b9bfa4e
Justin Smestad <justin.smestad@gmail.com>
2016-10-31 17:19:58 -0600

    page 183

--- rumbl/web/static/js/player.js
+++ rumbl/web/static/js/player.js
@@ -26,4 +26,5 @@ let Player = {
   getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000); },
   seekTo(millsec){ return this.player.seekTo(millsec / 1000); }
 };
+
 export default Player;
